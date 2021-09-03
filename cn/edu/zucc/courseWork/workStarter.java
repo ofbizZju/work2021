8fe188adb96a07d6ac452bfaf10652b147652305
ofbizZju <kehf@163.com>
2021-09-03 15:21:28 +0800

    Add files via upload

---
+++ cn/edu/zucc/courseWork/workStarter.java
@@ -0,0 +1,15 @@
+package cn.edu.zucc.courseWork;
+
+import cn.edu.zucc.courseWork.ui.FrmMain;
+
+public class workStarter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new FrmMain();
+	}
+
+}
